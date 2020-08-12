75cf2ddf8657c97c70357f34149deb6f7cc63b4d
firdausthedev <firdausthedev@gmail.com>
2020-08-13 03:49:08 +0800

    get notes from localstorage

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const addBtnModal = document.querySelector('#modal-form button')
 const titleInput = document.querySelector('#modal-form-title')
 const contentInput = document.querySelector('#modal-form-content')
 
+const circles = ['circle-red', 'circle-blue', 'circle-pink', 'circle-yellow', 'circle-green']
+
 const getSavedNotes = () => {
   const notesJSON = localStorage.getItem('notes')
   try {
@@ -14,7 +16,37 @@ const getSavedNotes = () => {
   }
 }
 
+const createNewNoteElements = (note) => {
+  baseNote = document.createElement('div')
+  baseNote.className = 'note shadow'
+
+  titleNote = document.createElement('div')
+  titleNote.className = 'note-title'
+
+  cicleTitleNote = document.createElement('div')
+  cicleTitleNote.className = `circle ${note.tag}`
+
+  textTitleNote = document.createElement('h2')
+  textTitleNote.appendChild(document.createTextNode(note.title))
+
+  titleNote.appendChild(cicleTitleNote)
+  titleNote.appendChild(textTitleNote)
+
+  contentNote = document.createElement('p')
+  contentNote.appendChild(document.createTextNode(note.content))
+  contentNote.className = 'note-para'
+
+  baseNote.appendChild(titleNote)
+  baseNote.appendChild(contentNote)
+
+  document.querySelector('#notes').appendChild(baseNote)
+}
+
 const notes = getSavedNotes()
+
+notes.forEach((note) => {
+  createNewNoteElements(note)
+})
 
 addBtn.addEventListener('click', (event) => {
   addNoteModal.style.display = 'block'
@@ -27,10 +59,12 @@ closeBtnModal.addEventListener('click', (event) => {
 addBtnModal.addEventListener('click', () => {
   const noteTitle = titleInput.value
   const noteContent = contentInput.value
-  const newNote = { title: noteTitle, content: noteContent }
+  const randomCircle = Math.floor(Math.random() * circles.length)
+  const newNote = { title: noteTitle, content: noteContent, tag: circles[randomCircle] }
 
   notes.push(newNote)
-  window.localStorage.setItem('notes', JSON.stringify(notes))
+  localStorage.setItem('notes', JSON.stringify(notes))
+  createNewNoteElements(newNote)
 
   titleInput.value = ''
   contentInput.value = ''
